5411d7ae2f62112e4f85cc54c3c30c677d371051
Craig S. Cottingham <craig@cottingham.net>
2021-12-10 00:31:05 -0600

    feat: solve 2021 day 10 part 1

--- years/2021/10/index.ts
+++ years/2021/10/index.ts
@@ -1,9 +1,8 @@
-import _ from "lodash"
-import * as util from "../../../util/util"
-import * as test from "../../../util/test"
 import chalk from "chalk"
-import { log, logSolution, trace } from "../../../util/log"
 import { performance } from "perf_hooks"
+import { log, logSolution } from "../../../util/log"
+import * as test from "../../../util/test"
+import * as util from "../../../util/util"
 
 const YEAR = 2021
 const DAY = 10
@@ -12,21 +11,87 @@ const DAY = 10
 // data path    : /Users/ccottingham/Projects/advent-of-code/2021/years/2021/10/data.txt
 // problem url  : https://adventofcode.com/2021/day/10
 
+const illegalScore: Map<string, number> = new Map([
+	[")", 3],
+	["]", 57],
+	["}", 1197],
+	[">", 25137]
+])
+
 async function p2021day10_part1(input: string, ...params: any[]) {
-	return "Not implemented"
+	return util.lineify(input.trim()).reduce((acc, line) => acc + scoreForLine(line), 0)
 }
 
 async function p2021day10_part2(input: string, ...params: any[]) {
 	return "Not implemented"
 }
 
+// The only characters in the lines are opening and closing characters, which means that at the innermost
+// nesting of a valid chunk there will be one or more adjacent opening/closing characters. So we can remove
+// adjacent opening/closing characters until the line is unchanged, then the resulting line will start
+// with a corrupted chunk (if any). The corrupted closing character will be the first closing character in
+// the line.
+//
+// For simplicity, rather than loop until the line is unchanged, just loop (line.length) times. That's a
+// guaranteed hard limit on the number of opening/closing character pairs we can remove (the limit is
+// actually (line.length / 2), but I don't want to mess with rounding up if the line length is an odd number,
+// and lines should be short enough that twice too many iterations will take a long time).
+//
+// If the line is incomplete rather than corrupted, there will be no closing character, so return 0.
+
+function scoreForLine(line: string): number {
+	for (let i = line.length; i; --i) {
+		line = line.replaceAll(/\(\)/g, "")
+		line = line.replaceAll(/\[\]/g, "")
+		line = line.replaceAll(/\{\}/g, "")
+		line = line.replaceAll(/\<\>/g, "")
+	}
+
+	const badIndex = line.search(/[\)\]\}\>]/)
+	if (badIndex > -1) {
+		return illegalScore.get(line.charAt(badIndex)) || 0
+	} else {
+		return 0
+	}
+}
+
 async function run() {
 	const testData = `
-`
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+	`
 	const part1tests: TestCase[] = [
 		{
+			input: "{([(<{}[<>[]}>{[]{[(<()>",
+			expected: "1197"
+		},
+		{
+			input: "[[<[([]))<([[{}[[()]]]",
+			expected: "3"
+		},
+		{
+			input: "[{[{({}]{}}([{[{{{}}([]",
+			expected: "57"
+		},
+		{
+			input: "[<(<(<(<{}))><([]([]()",
+			expected: "3"
+		},
+		{
+			input: "<{([([[(<>()){}]>(<<{{",
+			expected: "25137"
+		},
+		{
 			input: testData,
-			expected: ""
+			expected: "26397"
 		}
 	]
 	const part2tests: TestCase[] = [
