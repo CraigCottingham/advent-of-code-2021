95fe37a07882683f2264b5ce0d2458c9820af7d2
Craig S. Cottingham <craig@cottingham.net>
2021-12-10 23:37:49 -0600

    feat: solve 2021 day 11 part 1

--- years/2021/11/index.ts
+++ years/2021/11/index.ts
@@ -1,5 +1,6 @@
 import chalk from "chalk"
 import { performance } from "perf_hooks"
+import { Cell, Grid } from "../../../util/grid"
 import { log, logSolution } from "../../../util/log"
 import * as test from "../../../util/test"
 import * as util from "../../../util/util"
@@ -12,20 +13,84 @@ const DAY = 11
 // problem url  : https://adventofcode.com/2021/day/11
 
 async function p2021day11_part1(input: string, ...params: any[]) {
-	return "Not implemented"
+	const grid = new Grid({ serialized: input.trim() })
+	const iterations = params[0] || 100
+	let flashes = 0
+
+	for (let i = 0; i < iterations; ++i) {
+		for (const cell of grid) {
+			incrementCell(cell)
+		}
+
+		const flashed = grid.getCells("*")
+		flashes += flashed.length
+
+		for (const cell of flashed) {
+			cell.setValue("0")
+		}
+	}
+
+	return flashes
 }
 
 async function p2021day11_part2(input: string, ...params: any[]) {
 	return "Not implemented"
 }
 
+function incrementCell(cell: Cell): Cell {
+	const value = cell.value
+	if (value !== "*") {
+		let newValue = Number(value) + 1
+		cell.setValue(newValue.toString())
+		if (newValue > 9) {
+			cell.setValue("*")
+			for (const neighbor of cell.neighbors(true)) {
+				incrementCell(neighbor)
+			}
+		}
+	}
+	return cell
+}
+
 async function run() {
 	const testData = `
+5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526
 `
 	const part1tests: TestCase[] = [
 		{
+			input: `
+11111
+19991
+19191
+19991
+11111
+			`,
+			expected: "9",
+			extraArgs: [ 1 ]		// iterations
+		},
+		{
+			input: `
+11111
+19991
+19191
+19991
+11111
+			`,
+			expected: "34",
+			extraArgs: [ 10 ]		// iterations
+		},
+		{
 			input: testData,
-			expected: ""
+			expected: "1656"
 		}
 	]
 	const part2tests: TestCase[] = [
