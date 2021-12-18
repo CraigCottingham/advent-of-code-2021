299e7bd1119d3739c4f7fb9f80bd78e0a9c2a727
Craig S. Cottingham <craig@cottingham.net>
2021-12-18 01:13:16 -0600

    feat: solve 2021 day 16 part 2

--- years/2021/16/index.ts
+++ years/2021/16/index.ts
@@ -33,26 +33,84 @@ type Operator = Packet & {
 }
 
 async function p2021day16_part1(input: string, ...params: any[]) {
-	const data = util.lineify(input.trim())
-	const bitstream = data[0].split("").map((digit) => digitToBinary(digit)).join("").split("")
-	const packets = []
-
-	// this would be an opportunity to define an iterator on the bitstream, wouldn't it?
-	// basically, read packets off of bitstream until empty and return a packet array
-
-	while (bitstream.length > 0) {
-		packets.push(readPacket(bitstream))
-	}
-
-	return versionSum(packets)
+	return versionSum(parseBitstream(input))
 }
 
 async function p2021day16_part2(input: string, ...params: any[]) {
-	return "Not implemented"
+	return evaluate(parseBitstream(input))
 }
 
 function binaryToNumber(bits: string[]): number {
 	return Number.parseInt(bits.join(""), 2)
+}
+
+function decompile(packet: NullablePacket): string {
+	let output = ""
+
+	if (packet === null) {
+		output = chalk.white("null")
+	} else {
+		switch (packet.typeID) {
+			case 0:		// sum
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( + ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 1:   // prod
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( * ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 2:		// min
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( min ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 3:		// max
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( max ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 4:		// literal
+				output = chalk.greenBright((packet as Literal).value)
+				break
+
+			case 5:		// greater than?
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( gt ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 6:		// less than?
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( lt ${subpackets.join(" ")} )`)
+				}
+				break
+
+			case 7:		// equal?
+				{
+					let subpackets = (packet as Operator).packets.map((packet: NullablePacket) => decompile(packet))
+					output = chalk.blueBright(`( eq ${subpackets.join(" ")} )`)
+				}
+				break
+
+			default:	// unknown
+				output = chalk.redBright("?")
+				break
+		}
+	}
+
+	return output
 }
 
 function digitToBinary(digit: string): string {
@@ -105,6 +163,103 @@ function digitToBinary(digit: string): string {
 		default:
 			return "0000"
 	}
+}
+
+function evaluateOne(packet: Packet): Literal {
+	switch (packet.typeID) {
+		case 0:		// sum
+			{
+				const operands = (packet as Operator).packets
+				const value = operands.map((p) => evaluateOne(p!)).reduce((acc, p) => acc + (p.value === undefined ? 0 : p.value), 0)
+				return makeLiteral(value)
+			}
+
+		case 1:		// product
+			{
+				const operands = (packet as Operator).packets
+				const value = operands.map((p) => evaluateOne(p!)).reduce((acc, p) => acc * (p.value === undefined ? 1 : p.value), 1)
+				return makeLiteral(value)
+			}
+
+		case 2:		// minimum
+			{
+				const operands = (packet as Operator).packets
+				return makeLiteral(util.min(operands, (p) => evaluateOne(p!).value!).value)
+			}
+
+		case 3:		// maximum
+			{
+				const operands = (packet as Operator).packets
+				return makeLiteral(util.max(operands, (p) => evaluateOne(p!).value!).value)
+			}
+
+		case 4:		// literal
+			return packet as Literal
+
+		case 5:		// greater than?
+			{
+				const operands = (packet as Operator).packets
+				const lhs = evaluateOne(operands[0]!).value!
+				const rhs = evaluateOne(operands[1]!).value!
+				return makeLiteral((lhs > rhs) ? 1 : 0)
+			}
+
+		case 6:		// less than?
+			{
+				const operands = (packet as Operator).packets
+				const lhs = evaluateOne(operands[0]!).value!
+				const rhs = evaluateOne(operands[1]!).value!
+				return makeLiteral((lhs < rhs) ? 1 : 0)
+			}
+
+		case 7:		// equal?
+			{
+				const operands = (packet as Operator).packets
+				const lhs = evaluateOne(operands[0]!).value!
+				const rhs = evaluateOne(operands[1]!).value!
+				return makeLiteral((lhs == rhs) ? 1 : 0)
+			}
+
+		default:
+			return makeLiteral(0)
+	}
+}
+
+function evaluate(packets: NullablePacket[]): number {
+	return packets.reduce((acc, packet) => {
+		if (packet === null) {
+			return acc
+		}
+		acc.push(evaluateOne(packet).value!)
+		return acc
+	}, [] as number[]).shift()!
+}
+
+function makeLiteral(value: number): Literal {
+	return {
+		version: 0,
+		typeID: 4,
+		value
+	}
+}
+
+function parseBitstream(input: string): NullablePacket[] {
+	const data = util.lineify(input.trim())
+	const bitstream = data[0].split("").map((digit) => digitToBinary(digit)).join("").split("")
+	const packets = []
+
+	// this would be an opportunity to define an iterator on the bitstream, wouldn't it?
+	// basically, read packets off of bitstream until empty and return a packet array
+
+	while (bitstream.length > 0) {
+		packets.push(readPacket(bitstream))
+	}
+
+	if (packets.at(-1) === null) {
+		packets.pop()
+	}
+
+	return packets
 }
 
 function readLiteral(bitstream: string[]): Literal {
@@ -193,8 +348,6 @@ function versionSum(packets: NullablePacket[]): number {
 }
 
 async function run() {
-	const testData = `
-`
 	const part1tests: TestCase[] = [
 		{
 			input: "8A004A801A8002F478",
@@ -215,8 +368,64 @@ async function run() {
 	]
 	const part2tests: TestCase[] = [
 		{
-			input: testData,
-			expected: ""
+			// ( + 1 2 )
+			input: "C200B40A82",
+			expected: "3"
+		},
+		{
+			// ( * 6 9 )
+			input: "04005AC33890",
+			expected: "54"
+		},
+		{
+			// ( min 7 8 9 )
+			input: "880086C3E88112",
+			expected: "7"
+		},
+		{
+			// ( max 7 8 9 )
+			input: "CE00C43D881120",
+			expected: "9"
+		},
+		{
+			// ( lt 5 15 )
+			input: "D8005AC2A8F0",
+			expected: "1"
+		},
+		{
+			// ( gt 5 15 )
+			input: "F600BC2D8F",
+			expected: "0"
+		},
+		{
+			// ( eq 5 15 )
+			input: "9C005AC2F8F0",
+			expected: "0"
+		},
+		{
+			// ( eq ( + 1 3 ) ( * 2 2 ) )
+			input: "9C0141080250320F1802104A08",
+			expected: "1"
+		},
+		{
+			// ( + 192 )
+			input: "0000404E00",
+			expected: "192"
+		},
+		{
+			// ( eq 16 30 )
+			input: "1E008488048B80",
+			expected: "0"
+		},
+		{
+			// ( * 0 817 )
+			input: "06008400939840",
+			expected: "0"
+		},
+		{
+			// ( * ( eq 16 30 ) 817 )
+			input: "0600878021220122E1273080",
+			expected: "0"
 		}
 	]
 
