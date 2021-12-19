6e3b0a4c473b9c532efba46bdbb44b0a66287b3a
Craig S. Cottingham <craig@cottingham.net>
2021-12-18 21:53:18 -0600

    feat: solve 2021 day 17 part 1

--- years/2021/17/index.ts
+++ years/2021/17/index.ts
@@ -1,9 +1,8 @@
-import _ from "lodash"
-import * as util from "../../../util/util"
-import * as test from "../../../util/test"
 import chalk from "chalk"
-import { log, logSolution, trace } from "../../../util/log"
 import { performance } from "perf_hooks"
+import { log, logSolution } from "../../../util/log"
+import * as test from "../../../util/test"
+import * as util from "../../../util/util"
 
 const YEAR = 2021
 const DAY = 17
@@ -12,8 +11,69 @@ const DAY = 17
 // data path    : /Users/ccottingham/Documents/Projects/advent-of-code/2021/years/2021/17/data.txt
 // problem url  : https://adventofcode.com/2021/day/17
 
+type Point = { x: number, y: number }
+type Area = { topLeft: Point, bottomRight: Point }
+type Velocity = { dx: number, dy: number }
+
+function inTargetArea(p: Point, area: Area): boolean {
+  return ((p.x >= area.topLeft.x) && (p.x <= area.bottomRight.x)) &&
+				 ((p.y <= area.topLeft.y) && (p.y >= area.bottomRight.y))
+}
+
+function track(vi: Velocity, area: Area): Point[] {
+	let point: Point = { x: 0, y: 0 }
+	let velocity: Velocity = vi
+	const points: Point[] = []
+
+	while ((point.x <= area.bottomRight.x) && (point.y >= area.bottomRight.y)) {
+		points.push({ x: point.x, y: point.y })
+
+		point.x += velocity.dx
+		point.y += velocity.dy
+
+		velocity.dx = (velocity.dx === 0) ? 0 : (velocity.dx - 1)
+		velocity.dy -= 1
+	}
+
+	return points
+}
+
+// Testing a single [vx, vy] pair probably doesn't take long, so we can just brute force it.
+// However, I can think of some ways to minimize the problem space. I'll try them later.
+
 async function p2021day17_part1(input: string, ...params: any[]) {
-	return "Not implemented"
+  const match = util.regexify(input.trim(), /x=([-\d]+)\.\.([-\d]+),\s+y=([-\d]+)\.\.([-\d]+)/)
+	if (!match) {
+		return "wrong regex for the input data"
+	}
+
+	const x1: number = Number(match[1])
+	const x2: number = Number(match[2])
+	const y1: number = Number(match[3])
+	const y2: number = Number(match[4])
+
+	const targetArea = {
+		topLeft: {
+			x: Math.min(x1, x2),
+			y: Math.max(y1, y2)
+		},
+		bottomRight: {
+			x: Math.max(x1, x2),
+			y: Math.min(y1, y2)
+		}
+	}
+
+	const ys: number[] = []
+	for (let x = 1; x <= targetArea.bottomRight.x; ++x) {
+		for (let y = 1; y <= 200; ++y) {
+			const points = track({ dx: x, dy: y }, targetArea)
+			if (points.some((point) => inTargetArea(point, targetArea))) {
+				ys.push(...points.map((point) => point.y))
+			}
+		}
+	}
+
+	return util.max(ys).elem
 }
 
 async function p2021day17_part2(input: string, ...params: any[]) {
@@ -22,11 +82,12 @@ async function p2021day17_part2(input: string, ...params: any[]) {
 
 async function run() {
 	const testData = `
+target area: x=20..30, y=-10..-5
 `
 	const part1tests: TestCase[] = [
 		{
 			input: testData,
-			expected: ""
+			expected: "45"
 		}
 	]
 	const part2tests: TestCase[] = [
